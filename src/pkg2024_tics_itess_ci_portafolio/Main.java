e81e86ff5042a1c8b38202d334c705463eb2cceb
Vanesa <vanegoho@gmail.com>
2024-05-24 13:57:00 -0600

    Inicio de proyecto.

---
+++ src/pkg2024_tics_itess_ci_portafolio/Main.java
@@ -0,0 +1,22 @@
+
+package pkg2024_tics_itess_ci_portafolio;
+
+/**
+ * ITESS 
+ * TI202
+ * PORTAFOLIO DE EVIDENCIAS
+ * ENERO-JUNIO
+ * CALCULO 
+ *  Vanesa Honorato
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+       
+        
+        
+        
+    }
+    
+}
